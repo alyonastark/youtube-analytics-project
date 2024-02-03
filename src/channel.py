64026e9af0f21652fe1d404c8df57a06984fd1a3
Alyona Starkova <alyona.starrkova@yandex.ru>
2024-02-04 02:03:04 +0500

    Add atributes and methods to class Channel

--- src/channel.py
+++ src/channel.py
@@ -12,9 +12,66 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         print(channel)
+
+    @property
+    def title(self):
+        """Геттер для получения названия канала"""
+        return self.channel['items'][0]['snippet']['title']
+
+    @property
+    def channel_id(self):
+        """Геттер для получения id канала"""
+        return self.channel['items'][0]['id']
+
+    @property
+    def channel_description(self):
+        """Геттер для получения описания канала"""
+        return self.channel['items'][0]['snippet']['description']
+
+    @property
+    def url(self):
+        """Геттер для получения url канала"""
+        return self.channel['items'][0]['snippet']['thumbnails']['default']['url']
+
+    @property
+    def subscriber_count(self):
+        """Геттер для получения количества подписчиков канала"""
+        return self.channel['items'][0]['statistics']['subscriberCount']
+
+    @property
+    def video_count(self):
+        """Геттер для получения количества видео на канале"""
+        return self.channel['items'][0]['statistics']['videoCount']
+
+    @property
+    def view_count(self):
+        """Геттер для получения количества просмотров канала"""
+        return self.channel['items'][0]['statistics']['viewCount']
+
+    @classmethod
+    """Возвращает объект для работы с Youtube API"""
+    def get_service(cls):
+        return cls.youtube
+
+    def to_json(self, file_name='moscowpython.json'):
+        """Сохраняет в файл значения атрибутов экземпляра Channel"""
+        with open(file_name, 'w', encoding='utf-8') as file:
+            json.dump(self.channel, file_name)
+
+
+
+
+
+
+
+
+
+
