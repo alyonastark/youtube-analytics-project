7eebc8e74ec851684d2dda898effb4326eeb7089
Alyona Starkova <alyona.starrkova@yandex.ru>
2024-02-07 19:17:11 +0500

    Add str and comparison methods to class Channel

--- src/channel.py
+++ src/channel.py
@@ -15,6 +15,37 @@ class Channel:
         self.__channel_id = channel_id
         self.channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
 
+    def __str__(self):
+        """Возвращает название и адрес канала"""
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        """Складывает количество подписчиков каналов"""
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        """Метод для вычитания, показывает разницу между количеством подписчиков"""
+        return self.subscriber_count - other.subscriber_count
+
+    def __gt__(self, other):
+        """Метод для сравнения, возвращает True, если количество подписчиков первого канала больше чем у другого"""
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        """Метод для сравнения, возвращает True, если количество подписчиков первого канала больше или равно другому"""
+        return self.subscriber_count >= other.subscriber_count
+
+    def __lt__(self, other):
+        """Метод для сравнения, возвращает True, если количество подписчиков первого канала меньше чем у другого"""
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        """Метод для сравнения, возвращает True, если количество подписчиков первого канала меньше или равно другому"""
+        return self.subscriber_count <= other.subscriber_count
+
+    def __eq__(self, other):
+        """Метод для сравнения, возвращает True, если количество подписчиков у двух канало одинаковое"""
+        return self.subscriber_count == other.subscriber_count
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -39,12 +70,13 @@ class Channel:
     @property
     def url(self):
         """Геттер для получения url канала"""
-        return self.channel['items'][0]['snippet']['thumbnails']['default']['url']
+        channel_url = f"https://www.youtube.com/channel/{self.channel['items'][0]['id']}"
+        return channel_url
 
     @property
     def subscriber_count(self):
         """Геттер для получения количества подписчиков канала"""
-        return self.channel['items'][0]['statistics']['subscriberCount']
+        return int(self.channel['items'][0]['statistics']['subscriberCount'])
 
     @property
     def video_count(self):
