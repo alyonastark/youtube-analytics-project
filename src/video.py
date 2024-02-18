b8753d8c650d2aadedb0b003071103803cd93e28
Alyona Starkova <alyona.starrkova@yandex.ru>
2024-02-19 00:19:44 +0500

    add exception handling for broken video id

--- src/video.py
+++ src/video.py
@@ -11,29 +11,41 @@ class Video:
         self.video_id = video_id
         self.video = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
 
+
     def __str__(self):
         return self.video_title
 
     @property
     def video_title(self):
         """Геттер для получения названия видео"""
-        return self.video['items'][0]['snippet']['title']
-
+        try:
+            return self.video['items'][0]['snippet']['title']
+        except IndexError:
+            return None
     @property
     def video_view_count(self):
         """Геттер для получения количества просмотров видео"""
-        return self.video['items'][0]['statistics']['viewCount']
+        try:
+            return self.video['items'][0]['statistics']['viewCount']
+        except IndexError:
+            return None
 
     @property
     def video_like_count(self):
         """Геттер для получения количества лайков видео"""
-        return self.video['items'][0]['statistics']['likeCount']
+        try:
+            return self.video['items'][0]['statistics']['likeCount']
+        except IndexError:
+            return None
 
     @property
     def video_url(self):
         """Геттер для получения url видео"""
-        video_url = f"https://www.youtube.com/watch?v={self.video['items'][0]['id']}"
-        return self.video_url
+        try:
+            video_url = f"https://www.youtube.com/watch?v={self.video['items'][0]['id']}"
+            return self.video_url
+        except IndexError:
+            return None
 
 
 class PLVideo:
